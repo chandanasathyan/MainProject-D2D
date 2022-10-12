381e8332a1c03584b3f80b9c0bf9367ecc13a882
Ajay krishna <ajayajuu73@gmail.com>
2022-10-12 16:31:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/header/Header';
+import Home from './pages/home/Home';
+import Contact from './pages/contact/Contact';
+import Testmonials from './pages/testmonials/Testmonials';
+import Domain from './pages/domains/domain'
+import About from './pages/aboutus/About'
+
+
+function App() {
+  return (
+    <>
+<Header/>
+
+
+  <Routes>
+         <Route path="/" element={<Home/>} />
+ <Route path="/testimonials" element={<Testmonials/>} />  
+ <Route path="/domains" element={<Domain/>} />  
+ <Route path="/about" element={<About/>} />  
+ <Route path="/contact" element={<Contact/>} />  
+      </Routes> 
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./Header.css"
+import AppBar from '@mui/material/AppBar';
+import { Grid, Toolbar } from '@mui/material';
+
+const Header = () => {
+  return (
+    <>
+    
+    
+ <AppBar  sx={{background:"transparent",boxShadow:"none"}} className='navbar'  >
+        <Toolbar>
+        <Grid container spacing={2}  sx={{ display: { xs: 'none', sm: 'none', md: 'flex' ,lg:"flex" } }}>
+  
+        <Grid className='logoimg' item xs={1}>
+          {/* <img src={logo} alt="" srcset="" /> */} 
+        </Grid>
+  <Grid className='navlogo' item xs={2}>
+
+ 
+<p>  <Link className='navlogo1' to="/">D2D</Link></p>  
+
+
+  </Grid> 
+  <Grid style={{textDecoration:"none"}} className='navlink' item xs={7}>
+  <nav >
+
+<Link  style={{textDecoration:"none"}}  className='navlink1' to="/domains">DOMAINS</Link>  
+
+<Link  style={{textDecoration:"none"}}  className='navlink1' to="/testimonials">TESTIMONIALS</Link>  
+<Link  style={{textDecoration:"none"}}  className='navlink1' to="/about">ABOUT US</Link>  
+<Link  style={{textDecoration:"none"}}  className='navlink1' to="/contact">CONTACT US</Link>  
+
+</nav>
+  </Grid> 
+
+  </Grid>
+ </Toolbar>
+ </AppBar>
+
+
+
+
+    
+    
+    </>
+  )
+}
+
+export default Header
