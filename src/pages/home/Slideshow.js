a262fa46d39380de987898f05429dfcd32e9fca0
Ajay krishna <ajayajuu73@gmail.com>
2022-10-28 18:39:04 +0530

    28/10/2022 commit

--- src/pages/home/Slideshow.js
+++ src/pages/home/Slideshow.js
@@ -1,129 +1,195 @@
 import React from 'react'
 import "./Slideshow.css"
 import {Grid} from "@mui/material";
-// import webBan from "../../Assests/webBan-1.jpg"
-import webdev from "../../Assests/webdevelopment.jpg"
-import mobileapp from "../../Assests/mobileapp.jpg"
-import digitalmar from "../../Assests/digitalmarketing.jpg"
-import softtest from "../../Assests/softwaretesting.jpg"
-// import slide from '../../Assests/Slide-show-01.jpg'
-import slide from "../../Assests/slid-72.jpg"
-// import banner from "../../Assests/Banner-72-01.jpg"
-import banner from "../../Assests/Banner (1).png"
-import slide2 from "../../Assests/Mobile.png"
-import banweb from "../../Assests/SlideShow-Web.png"
+
+import banweb from "../../Assests/Slide-show-Web-view.png"
 import bantab from "../../Assests/Tab-View (1).png"
-import banmob from "../../Assests/Mob-view (1).png"
+import banmob from "../../Assests/Mob-view (3).png"
 import banwebmob from "../../Assests/Mobile-web-view.png"
 import bantabmob from "../../Assests/Mobile-tab-View.png"
-import banmobmob from"../../Assests/Mobile-Mob-view.png"
+import banmobmob from"../../Assests/Mobile-Mob-view (4).png"
+import dmweb from "../../Assests/DM-Web-view.png"
+import dmtab from "../../Assests/DM-Tab-View.png"
+import dmmob from"../../Assests/DM-Mob-view.png"
+import qaweb from "../../Assests/QA-Web-view.png"
+import qatab from "../../Assests/QA-Tab-View.png"
+import qamob from"../../Assests/QA-Mob-view.png"
 
 
-import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css"
+
 
 
 
 const Slideshow = () => {
+
+
+    const settings = {
+  
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 800,
+        autoplaySpeed: 3500,
+        pauseOnHover: false,
+        cssEase: "linear"
+        
+      };
+
+      const settingstab = {
+  
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 500,
+        autoplaySpeed: 2500,
+        pauseOnHover: false,
+        cssEase: "linear"
+        
+      };
+
+
+      const settingsmob = {
+  
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 400,
+        autoplaySpeed: 2500,
+        pauseOnHover: false,
+        cssEase: "linear"
+        
+      };
+
   return (
     <>
-    
-    <Grid container  sx={{ display: { xs: 'none', sm:"none", md:"flex", lg: 'flex', xl: 'flex' } }}>
+
+   
+
+      <div style={{marginTop:"95px",overflow:"hidden"}}>
+ 
+     
+    <Grid container sx={{ display: { xs: 'none', sm:"none", md:"flex", lg: 'flex', xl: 'flex' } }} >
     <Grid item xs={12}>
-<div className='slideshow-main-div'>
+        <Slider {...settings} >
+          <div>
+      
+          <img className=' slideimage1' src={banweb} alt=""/>
+         
+          </div>
+          <div>
+      
+      <img className=' slideimage1' src={banwebmob} alt=""/>
+     
+      </div>
+      <div>
+      
+          <img className=' slideimage1' src={dmweb} alt=""/>
+         
+          </div>
+          <div>
+      
+      <img className=' slideimage1' src={qaweb} alt=""/>
+     
+      </div>
+       
+        </Slider>
+        </Grid>
+        </Grid>
 
-<div class="uk-position-relative uk-visible-toggle uk-light"  tabindex="-1" uk-slideshow="ratio: false; autoplay:false;autoplay-interval: 2000;pause-on-hover: false">
-
-    <ul class="uk-slideshow-items" uk-height-viewport="">
-        <li>
-            <img className=' slideimage1' src={banweb} alt="" uk-cover/>
-        </li>
-        <li>
-            <img className=' slideimage1' src={banwebmob} alt="" uk-cover/>
-        </li>
-        <li>
-            <img className=' slideimage1' src={digitalmar} alt="" uk-cover/>
-        </li>
-        <li>
-            <img className=' slideimage1' src={softtest} alt="" uk-cover/>
-        </li>
-    </ul>
-
-    <a style={{color:"#111",marginTop:"-80px"}} class="uk-position-center-left uk-position-small uk-hidden-hover" href="#q" uk-slidenav-previous uk-slideshow-item="previous"> <BsChevronLeft/> </a>
-    <a style={{color:"#111",marginTop:"-80px"}} class="uk-position-center-right uk-position-small uk-hidden-hover" href="#q" uk-slidenav-next uk-slideshow-item="next"> <BsChevronRight/> </a>
-
-</div>
-</div>
-
-</Grid>  
-</Grid>
 
 {/* tab */}
 
-<Grid container  sx={{ display: { xs: 'none', sm:"flex", md:"none", lg: 'none', xl: 'none' } }} >
+
+        <Grid container sx={{ display: { xs: 'none', sm:"flex", md:"none", lg: 'none', xl: 'none' } }}>
     <Grid item xs={12}>
-<div className='slideshow-main-div'>
+        <Slider {...settingstab}>
+          <div>
+      
+          <img className=' slideimage1'  src={bantab} alt=""/>
+         
+          </div>
+          <div>
+      
+      <img className=' slideimage1' src={bantabmob} alt=""/>
+     
+      </div>
+      <div>
+      
+          <img className=' slideimage1' src={dmtab} alt=""/>
+         
+          </div>
+          <div>
+      
+      <img className=' slideimage1' src={qatab} alt=""/>
+     
+      </div>
+       
+        </Slider>
+        </Grid>
+        </Grid>
 
-<div class="uk-position-relative uk-visible-toggle uk-light"  tabindex="-1" uk-slideshow="ratio: false; autoplay:false;autoplay-interval: 2000;pause-on-hover: false">
-
-    <ul class="uk-slideshow-items" uk-height-viewport="">
-        <li>
-            <img className=' slideimage1' src={bantab} alt="" uk-cover/>
-        </li>
-        <li>
-            <img className=' slideimage1' src={bantabmob} alt="" uk-cover/>
-        </li>
-        <li>
-            <img className=' slideimage1' src={digitalmar} alt="" uk-cover/>
-        </li>
-        <li>
-            <img className=' slideimage1' src={softtest} alt="" uk-cover/>
-        </li>
-    </ul>
-
-    <a style={{color:"#111",marginTop:"-80px"}} class="uk-position-center-left uk-position-small uk-hidden-hover" href="#q" uk-slidenav-previous uk-slideshow-item="previous"> <BsChevronLeft/> </a>
-    <a   style={{color:"#111",marginTop:"-80px"}} class="uk-position-center-right uk-position-small uk-hidden-hover" href="#q" uk-slidenav-next uk-slideshow-item="next"> <BsChevronRight/> </a>
-
-</div>
-
-
-</div> 
-</Grid>  
-</Grid>
-
-
-{/* mobile */}
+{/* mob */}
 
 
- <Grid container  sx={{ display: { xs: 'flex', sm:"none", md:"none", lg: 'none', xl: 'none' } }}>
+
+
+        <Grid container sx={{ display: { xs: 'flex', sm:"none", md:"none", lg: 'none', xl: 'none' } }}>
     <Grid item xs={12}>
-<div className='slideshow-main-div'>
+        <Slider {...settingsmob}>
+          <div>
+      
+          <img className=' slideimage1' src={banmob}  alt=""/>
+         
+          </div>
+          <div>
+      
+      <img className=' slideimage1'  src={banmobmob} alt=""/>
+     
+      </div>
+      <div>
+      
+          <img className=' slideimage1'  src={dmmob} alt=""/>
+         
+          </div>
+          <div>
+      
+      <img className=' slideimage1' src={qamob} alt=""/>
+     
+      </div>
+       
+        </Slider>
+        </Grid>
+        </Grid>
 
-<div class="uk-position-relative uk-visible-toggle uk-light"  tabindex="-1" uk-slideshow="ratio: false; autoplay:false;autoplay-interval: 2000;pause-on-hover: false">
-
-    <ul class="uk-slideshow-items" uk-height-viewport="">
-        <li>
-            <img className=' slideimage1' src={banmob} alt="" uk-cover/>
-        </li>
-        <li>
-            <img className=' slideimage1' src={banmobmob} alt="" uk-cover/>
-        </li>
-        <li>
-            <img className=' slideimage1' src={digitalmar} alt="" uk-cover/>
-        </li>
-        <li>
-            <img className=' slideimage1' src={softtest} alt="" uk-cover/>
-        </li>
-    </ul>
-
-    <a style={{color:"#111",marginTop:"-80px"}} class="uk-position-center-left uk-position-small uk-hidden-hover" href="#q" uk-slidenav-previous uk-slideshow-item="previous"> <BsChevronLeft/> </a>
-    <a   style={{color:"#111",marginTop:"-80px"}} class="uk-position-center-right uk-position-small uk-hidden-hover" href="#q" uk-slidenav-next uk-slideshow-item="next"> <BsChevronRight/> </a>
-
-</div>
 
 
-</div> 
-</Grid>  
-</Grid>
+      </div>
+    
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     
